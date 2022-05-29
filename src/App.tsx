7fe23481adeeb178b6c2c2ec74cc2dee5fa3aab4
Kaik Chaides Barreto Pinto <kaikchaides123@gmail.com>
2022-05-29 16:02:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
-  return 
+  return (
+      <p>Olá</p>
+  )
 }
 
 export default App;
